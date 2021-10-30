778210cc642bf7e527ea381e28303a5ef0a11fb4
odckumar <praveenebox96@gmail.com>
2021-10-30 12:18:47 +0530

    First

---
+++ src/org/com/Test.java
@@ -0,0 +1,17 @@
+package org.com;
+
+public class Test {
+	public static void main(String[] args) {
+		int a = 0;
+		for (int i = 1; i <= 50; i++) {
+			if (i % 2 != 0) {
+
+				a = a + 1;
+			}
+			System.out.println(a);
+
+		}
+	
+	}
+
+}
